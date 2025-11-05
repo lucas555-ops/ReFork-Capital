5790fb015b86174ceaff2176e1b16aa8443e521b
lucas555-ops <lucas9589716@gmail.com>
2025-11-06 00:40:56 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,3 +1,5 @@
+Проверь 
+
 module.exports = async function handler(req, res) {
   // ===== CORS НАСТРОЙКА =====
   const allowedOrigins = [
@@ -79,6 +81,8 @@ module.exports = async function handler(req, res) {
 👤 <b>Имя:</b> ${name}
 📱 <b>Telegram:</b> ${telegram}
 💰 <b>Пакет:</b> ${pkg}
+🌐 <b>Язык:</b> ${lang}
+📍 <b>Источник:</b> ${source}
 🕐 <b>Время:</b> ${new Date().toLocaleString('ru-RU')}
     `.trim();
 
