e33d0d7c68ffca10f57e7dee9ea648b5c03e9024
lucas555-ops <lucas9589716@gmail.com>
2025-11-06 02:16:52 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -7,8 +7,7 @@ export default async function handler(req, res) {
     "https://reforkcapital.online",
     "https://www.reforkcapital.online",
     "http://localhost:3000",
-    "https://re-fork-capital.vercel.app"
-    "https://lucas555-ops.github.io/ReFork-Capital/"
+    "https://re-fork-capital.vercel.app"    
   ];
 
   const origin = req.headers.origin || req.headers.referer;
