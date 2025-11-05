5c63d827cd1fd0809d063cc9c17f976d41d64092
lucas555-ops <lucas9589716@gmail.com>
2025-11-06 01:14:25 +0500

    Update telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // ===== CORS НАСТРОЙКА =====
   const allowedOrigins = [
     'https://lucas555-ops.github.io',
